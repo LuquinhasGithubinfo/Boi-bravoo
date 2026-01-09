2c058c8c0566a6bf1ee0709d55816a3d23ebcce9
wvitecnologia <lucassimpat@gmail.com>
2026-01-09 08:20:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-// Pequenas melhorias futuras (scroll suave, analytics, etc.)
-console.log("Boi Bravo site carregado com sucesso.");
+const obs = new IntersectionObserver(entries=>{
+  entries.forEach(e=>{
+    if(e.isIntersecting){
+      e.target.style.opacity=1;
+      e.target.style.transform="translateY(0)";
+    }
+  })
+})
+
+document.querySelectorAll("section,.product,blockquote").forEach(el=>{
+  el.style.opacity=0
+  el.style.transform="translateY(40px)"
+  el.style.transition="1s"
+  obs.observe(el)
+})
